a62acbdf588962f6a04ca2bf79d3b35bb93b0f11
“NeTimati” <nikita54591@gmail.com>
2023-09-27 09:39:16 +0300

    дз создать обработчики ошибок 500 403 400

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,14 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
 # модуль для хранения текущих представлений(то что мы будем видеть)
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Главная страница основного приложения')
+    res = request.GET
+    print(request.GET)
+    return HttpResponse(f'Главная страница основного приложения<br> {dict(res)["group"][0]}')
 
 def categorys(request):
     return HttpResponse('<h1> ссылки по категориям </h1>')
 
 def category(request, cat_id):
+    if cat_id >1000:
+        raise Http404()
     return HttpResponse(f'<h1> Номер категории - </h1> <br> {cat_id}')
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1> Страница не найдена </h1>')
