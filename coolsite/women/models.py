83f4ba72d45a9c792fa94d743d7d1e0276a080d4
VictoriaGailitis <heyleadrightnow@gmail.com>
2023-11-08 09:53:36 +0300

    1 создать таблицу любимых книг с полем их стоимости в издательстве

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -2,10 +2,13 @@ from django.db import models
 # для хранения ORM-моделей для представления данных из базы данных
 
 # Create your models here
+import datetime
+dt_now = datetime.datetime.now()
 
 class Students(models.Model):
     fio = models.CharField(max_length=30)
     interesting = models.TextField(blank=True)
+    birthday = models.DateTimeField(default=dt_now)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_money = models.BooleanField(default=True)
